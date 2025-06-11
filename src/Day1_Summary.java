39621a4939dcff83f63b5413bdcf85ea838ff041
VenkatKarthikeya09 <venkatkarthikeya.tadi@gmail.com>
2025-06-11 19:29:50 +0530

    Feat: Complete Day 1 assignment and exam

---
+++ src/Day1_Summary.java
@@ -0,0 +1,12 @@
+public class Day1_Summary {
+    public static void main(String[] args) {
+        System.out.println("""
+                *** Day 1 Learning Summary ***
+                1. JDK (Java Development Kit): This is the toolbox for developers. It includes the compiler (javac) to turn our .java files into .class files.
+                2. JRE (Java Runtime Environment): This is what's needed to RUN a Java program. It contains the JVM.
+                3. JVM (Java Virtual Machine): The 'magic' that runs our compiled Java code. It allows our code to be "Write Once, Run Anywhere."
+                4. IDE (IntelliJ IDEA): Our smart editor that helps us write, run, and debug code efficiently.
+                5. Git: Our Version Control System. It's like a time machine for our code, tracking every change.
+                6. Core Git Commands: I learned 'git status', 'git add', 'git commit', and 'git push'.""");
+    }
+}
